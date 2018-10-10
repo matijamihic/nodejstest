4ddf8e622cbc2a8cceeffc3599c1bc2599958bfc
Krilas <mihic.matija@gmail.com>
2018-10-10 09:13:12 +0000

    Router files and frontend for login form

---
+++ app/routes/router.js
@@ -0,0 +1,164 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+var path = require("path");
+var app = express();
+
+console.log('/home/ubuntu/workspace/templateLogReg/index.html');
+
+console.log(path.join(__dirname, '/templateLogRed/index.html'));
+
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/api', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/api/users')
+
+    // create a user (accessed at POST https://nodebeer-krilas.c9users.io/api/users)
+    .post(function(req, res) {
+
+        var user = new User();      // create a new instance of the User model
+        user.name = req.body.name;  // set the users name (comes from the request)
+        user.email = req.body.email;
+        user.password = req.body.password;
+        user.passwordConf = req.body.passwordConf;
+
+        // save the user and check for errors
+        user.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'User created!' });
+        });
+
+    })
+    
+    // get all the users (accessed at GET https://nodebeer-krilas.c9users.io/api/users)
+    .get(function(req, res) {
+        User.find(function(err, users) {
+            if (err)
+                res.send(err);
+
+            res.json(users);
+        });
+    });
+    
+// on routes that end in /users/:user_id
+// ----------------------------------------------------
+router.route('/api/users/:user_id')
+
+    // get the user with that id (accessed at GET https://nodebeer-krilas.c9users.io/api/users/:user_id)
+    .get(function(req, res) {
+        User.findById(req.params.user_id, function(err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    })
+    
+    
+    // update the user with this id (accessed at PUT https://nodebeer-krilas.c9users.io/api/users/:user_id)
+    .put(function(req, res) {
+
+        // use our user model to find the user we want
+        User.findById(req.params.user_id, function(err, user) {
+
+            if (err)
+                res.send(err);
+
+            user.name = req.body.name;  // update the users info
+
+            // save the user
+            user.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'User updated!' });
+            });
+
+        });
+    })
+    
+    // delete the user with this id (accessed at DELETE https://nodebeer-krilas.c9users.io/api/users/:user_id)
+    .delete(function(req, res) {
+        User.remove({
+            _id: req.params.user_id
+        }, function(err, user) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
+
+
+// GET route for reading data
+app.get('/', function (req, res, next) {
+  return res.sendFile(path.join(__dirname, 'templateLogRed'));
+  
+//  router.use(express.static(path.resolve(__dirname, 'client')));
+
+});
+
+//POST route for updating data
+router.post('/', function (req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords dont match");
+    return next(err);
+  }
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf,
+    }
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+
+  } else if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+})
+
+module.exports = router;
