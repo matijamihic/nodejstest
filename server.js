f7938b4eff67605343431d351f22dda476f1bfc6
Krilas <mihic.matija@gmail.com>
2018-10-10 09:05:54 +0000

    Moved all routes from server.js to app/routes/routes.js

--- server.js
+++ server.js
@@ -5,22 +5,32 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// call and connecto to external database
+// call and connecto to database
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://krilas-nodebeer-6431409:27017'); // connect to our database
+var db = mongoose.connection;
 var User     = require('./app/models/user');
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("we're connected!");
+});
 
 var port = process.env.PORT || 8080;        // set our port
 
 // include routes
 var routes = require('./app/routes/router');
-//app.use('/', routes);
+app.use('/', routes);
 
 // error handler
 // define as the last app.use callback
@@ -29,111 +39,15 @@ app.use(function (err, req, res, next) {
   res.send(err.message);
 });
 
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
-
-// on routes that end in /users
-// ----------------------------------------------------
-router.route('/users')
-
-    // create a user (accessed at POST https://nodebeer-krilas.c9users.io/api/users)
-    .post(function(req, res) {
-
-        var user = new User();      // create a new instance of the User model
-        user.name = req.body.name;  // set the users name (comes from the request)
-        user.email = req.body.email;
-        user.password = req.body.password;
-        user.passwordConf = req.body.passwordConf;
-
-        // save the user and check for errors
-        user.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'User created!' });
-        });
-
-    })
-    
-    // get all the users (accessed at GET https://nodebeer-krilas.c9users.io/api/users)
-    .get(function(req, res) {
-        User.find(function(err, users) {
-            if (err)
-                res.send(err);
-
-            res.json(users);
-        });
-    });
-    
-// on routes that end in /users/:user_id
-// ----------------------------------------------------
-router.route('/users/:user_id')
-
-    // get the user with that id (accessed at GET https://nodebeer-krilas.c9users.io/api/users/:user_id)
-    .get(function(req, res) {
-        User.findById(req.params.user_id, function(err, user) {
-            if (err)
-                res.send(err);
-            res.json(user);
-        });
-    })
-    
-    
-    // update the user with this id (accessed at PUT https://nodebeer-krilas.c9users.io/api/users/:user_id)
-    .put(function(req, res) {
-
-        // use our user model to find the user we want
-        User.findById(req.params.user_id, function(err, user) {
-
-            if (err)
-                res.send(err);
-
-            user.name = req.body.name;  // update the users info
-
-            // save the user
-            user.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'User updated!' });
-            });
-
-        });
-    })
-    
-    // delete the user with this id (accessed at DELETE https://nodebeer-krilas.c9users.io/api/users/:user_id)
-    .delete(function(req, res) {
-        User.remove({
-            _id: req.params.user_id
-        }, function(err, user) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Successfully deleted' });
-        });
-    });
-
-
-// REGISTER ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+//  store: new MongoStore({
+//    mongooseConnection: db
+//  })
+}));
 
 // START THE SERVER
 // =============================================================================
